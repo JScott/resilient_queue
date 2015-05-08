017d058b5eb517564962b62b1fec34f91ce5dc23
JScott <jvscott@gmail.com>
2015-05-08 09:54:18 -0700

    New add/remove design works

--- lib/resilient_queue.rb
+++ lib/resilient_queue.rb
@@ -8,28 +8,47 @@ class ResilientQueue
   def initialize(options = {})
     @name = options.fetch :name, 'default'
     @timeout = options.fetch :timeout, 60
-    @pending = "#{@name}:pending"
-    @claimed = "#{@name}:claimed"
     @db = Moneta.new :Daybreak, expires: true, file: ".#{@name}.db"
   end
 
-  def create_id
-    @db.increment "#{@name}:id_count"
+  def key_for(type, with_id: 0)
+    key = case type
+    when :pending_list
+      "#{@name}:pending"
+    when :claimed_list
+      "#{@name}:claimed"
+    when :claimed_flag
+      "#{@name}:task:#{with_id}:claimed"
+    when :item_store
+      "#{@name}:@{id}"
+    when :id_count
+      "#{@name}:id_count"
+    else
+    end
+    "#{@name}:#{key}"
   end
 
-  def key(id)
-    "#{@name}:@{id}"
+  def create_id
+    @db.increment key_for(:id_count)
+  end
+
+  def lookup(id)
+    @db.fetch key_for(:item_store, with_id: id), nil
   end
 
   def enqueue(item)
     id = create_id
-    @db.store key(id), item, expires: @timeout
-    @db.lpush @pending, id
+    @db.store key_for(:item_store, with_id: id), item
+    @db.lpush key_for(:pending_list), id
   end
 
   def dequeue
-    # TODO: requeuing
-    id = @db.rpoplpush @pending, @claimed
-    @db.fetch key(id)
+    process_expired_claims
+    id = @db.rpoplpush key_for(:pending_list), key_for(:claimed_list)
+    @db.store key_for(:claimed_flag, with_id: id), true, expires: @timeout
+    id
+  end
+
+  def process_expired_claims
   end
 end
